1c208037a29f32e51e7baf0e4f6b78a47368a7ec
Will DiBiagio <wdibiagio@gmail.com>
2019-12-15 18:39:54 -0800

    Completed day 2

---
+++ day-2/part_2.py
@@ -0,0 +1,42 @@
+class Intcode:
+    def __init__(self, data):
+        self.intList = data
+        self.index = 0
+
+    def positions(self):
+        return self.intList[self.index + 1], self.intList[self.index + 2], self.intList[self.index + 3]
+
+    def opcode_add(self):
+        p = self.positions()
+        self.intList[p[2]] = self.intList[p[0]] + self.intList[p[1]]
+        self.index += 4
+
+    def opcode_multiple(self):
+        p = self.positions()
+        self.intList[p[2]] = self.intList[p[0]] * self.intList[p[1]]
+        self.index += 4
+    
+    def run(self):
+        while self.current != 99:
+            if self.current == 1:
+                self.opcode_add()
+            elif self.current == 2:
+                self.opcode_multiple()
+        return self.intList
+
+    @property
+    def current(self):
+        return self.intList[self.index]
+
+def main():
+    for noun, verb in ((a, b) for a in range(100) for b in range(100)):
+        data = [int(i) for i in open("input.in").read().split(',')]
+        data[1] = noun
+        data[2] = verb
+        if Intcode(data).run()[0] == 19690720:
+            print(100 * noun + verb)
+            break
+
+
+if __name__ == "__main__":
+    main()
